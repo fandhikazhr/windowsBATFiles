c8bc61a28f8555f9f83352164e8a607ac6fdeedf
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-05-04 06:55:48 +0700

    Update playVideo.bat

--- playVideo.bat
+++ playVideo.bat
@@ -1,7 +1,11 @@
 @echo off
+:start
 title Play Video From Your Directory
 cd C:\Users\LAPTOP\Videos\
 dir
 set /p PLAY=Type to directory videos : 
 mpv %PLAY%
 echo Stop Playing Offline Video
+pause
+cls
+goto start
