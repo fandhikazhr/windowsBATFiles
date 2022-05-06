9d61a0f5608b4b80e6cc508e42e045dc8af54962
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-05-06 07:22:06 +0700

    Update playOnCMD.bat

--- playOnCMD.bat
+++ playOnCMD.bat
@@ -5,3 +5,11 @@ echo Play Video From :
 echo 1. Offline
 echo 2. Online
 set /p CHOOSE=(1 or 2) : 
+
+call :switch-case-N-%CHOOSE% 2>nul || (
+    :: Default case
+    echo Wrong choice
+    pause
+    cls
+    goto start
+)
