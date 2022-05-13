7a69c776d3d258a1f765a1ef6a379f3f11b5ea95
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-05-14 01:45:53 +0700

    Update iptvCMD.bat

--- iptvCMD.bat
+++ iptvCMD.bat
@@ -60,3 +60,4 @@ call :switch-case-N-%CHOOSE% 2>nul || (
 :switch-case-N-7
   mpv http://free.fullspeed.tv/iptv-query?streaming-ip=https://youtube.com/watch?v=K9lOPKwo44w
   echo Stop Playing
+  pause
