4c52a3e009cc2fc53cb8c8fffe359269ab207ac1
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-05-15 08:14:41 +0700

    Update playOnCMD.bat

--- playOnCMD.bat
+++ playOnCMD.bat
@@ -4,6 +4,8 @@ title Player CMD
 echo 8888888b.  888                         .d88888b.                 .d8888b.  888b     d888 8888888b.  
 echo 888   Y88b 888                        d88P" "Y88b               d88P  Y88b 8888b   d8888 888  "Y88b 
 echo 888    888 888                        888     888               888    888 88888b.d88888 888    888 
+echo 888   d88P 888  8888b.  888  888      888     888 88888b.       888        888Y88888P888 888    888 
+echo 8888888P"  888     "88b 888  888      888     888 888 "88b      888        888 Y888P 888 888    888 
 echo Play Video From :
 echo 1. Offline
 echo 2. Online
