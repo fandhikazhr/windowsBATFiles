d09e07b196d303f95e2ada5a52fc47c6b5b575a9
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-07-14 09:59:13 +0700

    Update iptv.bat

--- iptv.bat
+++ iptv.bat
@@ -1623,3 +1623,9 @@ call :switch-case-K-%CHOOSE% 2>nul || (
     pause
     cls
     goto education
+  :switch-case-W-14
+    mpv https://ntv1.akamaized.net/hls/live/2014075/NASA-NTV1-HLS/master.m3u8
+    echo Stop Playing
+    pause
+    cls
+    goto education
